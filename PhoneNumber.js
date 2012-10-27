13e2ab865e1a85038eccc613a1f6bb724347a636
Andreas Gal <andreas.gal@gmail.com>
2012-10-27 13:23:58 -0700

    remove load command for meta data

--- PhoneNumber.js
+++ PhoneNumber.js
@@ -1,8 +1,6 @@
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 
-load("PhoneNumberMetaData.js");
-
 var PhoneNumber = (function (dataBase) {
   const FILLER_CHARS = /#*()-\s/g;
   const PLUS_CHARS = /^\++/g;
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 
+load("PhoneNumberMetaData.js");
 load("PhoneNumber.js");
 
 function Test(currentRegion, dial, region, number, nationalFormat, internationalFormat) {
